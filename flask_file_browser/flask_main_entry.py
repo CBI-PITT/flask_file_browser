488264fca4241c6ea0d5ea86b27dc439036f9454
collin2046 <collin9527@gmail.com>
2025-02-17 11:20:47 -0500

    update clickboard method

--- flask_file_browser/flask_main_entry.py
+++ flask_file_browser/flask_main_entry.py
@@ -4,7 +4,7 @@ from routes import init_blueprint  # Import the init function
 
 app = Flask(__name__)
 # Register the blueprint with settings
-init_blueprint(app, prefix='/browser')
+app = init_blueprint(app, prefix='/browser')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
